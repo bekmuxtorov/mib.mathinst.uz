782965048f4239f6c02ed04591fdbb9481084626
bekmuxtorov <asadbekmuxtorov2@gmail.com>
2023-02-09 12:08:59 +0500

    🔗 Setup project

--- config/settings.py
+++ config/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env.str("DB_NAME"),
+        'USER': env.str("DB_USER"), 
+        'PASSWORD': env.str("DB_PASSWORD"),
+        'HOST': '127.0.0.1', 
+        'PORT': '5432',
     }
 }
 
